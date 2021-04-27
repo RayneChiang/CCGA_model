d2a200046e75f46c4edacfe95c5280e4ce14a1d6
Runying Jiang <raynechiang5235@gmail.com>
2021-04-27 01:38:01 +0100

    [update the way to represent a string and mutate]

--- main.py
+++ main.py
@@ -9,36 +9,45 @@ from decimal import Decimal
 
 
 class IndiString:
-    def __init__(self, min_v, max_v):
-        self.value = round(random.uniform(min_v, max_v), 10)
-        self.represent = self.__initStringValue__()
-
-    def __setRepresent__(self, string):
+    def __init__(self, string, divide):
         self.represent = string
+        self.divide_point = divide
+        self.value = self.__setValue__()
 
-        self.value = int(self.represent, 2) / math.pow(10, decimal_point)
-        self.__checkValue__()
+    def __setValue__(self):
+        num = int(self.represent[1:17], 2) / math.pow(2, self.divide_point)
+        if self.represent[0] == 0:
+            num = - num
+        while num > max_value or num < min_value:
+            num = num / 2
+            self.divide_point = self.divide_point + 1
+        return num
 
-    def __checkValue__(self):
-        while self.value > max_value or self.value < min_value:
-                self.value = self.value/5
-
-    def __initStringValue__(self):
-        number = int(self.value * math.pow(10, decimal_point))
-        while number > 65536 or number < -65536:
-            number = int(number/10)
-        return bin(number)
+    def __getValue__(self):
+        num = int(self.represent[1:17], 2) / math.pow(2, self.divide_point)
+        if self.represent[0] == 0:
+            num = - num
+        return num
 
 
-def init_pop(population_size, min_v, max_v, n):
+def init_pop(population_size, n):
     pop_list = []
     while len(pop_list) < population_size:
         pop_individual = []
         while len(pop_individual) < n:
-            new_indi = IndiString(min_v=min_v, max_v=max_v)
+            new_indi = IndiString(initStringValue(), 0)
             pop_individual.append(new_indi)
         pop_list.append(pop_individual)
     return pop_list
+
+
+def initStringValue():
+    seed = "01"
+    sa = []
+    for i in range(17):
+        sa.append(seed[np.random.randint(0, 2)])
+    salt = ''.join(sa)
+    return salt
 
 
 def get_random(key_list):
@@ -47,71 +56,62 @@ def get_random(key_list):
     return key_list[i]
 
 
-def crossover_mute(func, init_list, crossover_rate):
-    individual_A = get_random(init_list)
-    individual_B = get_random(init_list)
-    individual_C = get_random(init_list)
-    individual_D = get_random(init_list)
+def crossover_mute(func, list, crossover_rate):
+    individual_A = get_random(list)
+    individual_B = get_random(list)
+    individual_C = get_random(list)
+    individual_D = get_random(list)
     parent_A = individual_A if func(individual_A) < func(individual_B) else individual_B
     parent_B = individual_C if func(individual_C) < func(individual_D) else individual_D
+
+    child_list = []
     if random.uniform(0, 1) < crossover_rate:
-        point_one = random.randint(0, num_parameter+1)
-        point_two = random.randint(point_one, num_parameter+1)
-        child_list = []
-        if random.uniform(0, 1) < crossover_rate:
-            for i in range(len(parent_A)):
-                str_len = min((len(parent_A[i].represent), len(parent_B[i].represent)))
-                new_indi = IndiString(min_v=min_value, max_v=max_value)
-                point_one = random.randint(3, str_len)
-                point_two = random.randint(point_one, str_len)
-                new_string = parent_A[i].represent[0:point_one] + parent_B[i].represent[point_one:point_two] + \
-                             parent_A[i].represent[point_two:]
-                if len(new_string) > 16:
-                    new_indi.__setRepresent__(new_string)
-                    mutate(new_indi)
-                    child_list.append(new_indi)
-                else:
-                    child_list.append(parent_A[i])
-            return child_list
-        else:
-            return parent_A[0:point_one] + parent_B[point_one:point_two] + parent_A[point_two:]
+        for i in range(len(parent_A)):
+            str_len = min((len(parent_A[i].represent), len(parent_B[i].represent)))
+            point_one = random.randint(0, str_len)
+            point_two = random.randint(point_one, str_len)
+            new_string = parent_A[i].represent[0:point_one] + parent_B[i].represent[point_one:point_two] + \
+                         parent_A[i].represent[point_two:]
+            new_string = mutate(new_string)
+            new_indi = IndiString(new_string, parent_A[i].divide_point)
+            child_list.append(new_indi)
+        return child_list
     else:
-        return parent_A if func(parent_A) < func(parent_B) else parent_B
+        child = parent_A if func(parent_A) < func(parent_B) else parent_B
+        return child
 
 
-def crossover_mute_ccga(func, init_list, crossover_rate):
-    individual_A = get_random(init_list)
-    individual_B = get_random(init_list)
-    individual_C = get_random(init_list)
-    individual_D = get_random(init_list)
+def crossover_mute_ccga(func, list, crossover_rate):
+    individual_A = get_random(list)
+    individual_B = get_random(list)
+    individual_C = get_random(list)
+    individual_D = get_random(list)
     parent_A = individual_A if func(individual_A) < func(individual_B) else individual_B
     parent_B = individual_C if func(individual_C) < func(individual_D) else individual_D
     string_len = len(parent_A[0].represent)
-    if random.uniform(0, 1) < crossover_rate and string_len>16:
-        point_one = random.randint(3, string_len)
+    if random.uniform(0, 1) < crossover_rate:
+        point_one = random.randint(0, string_len)
         point_two = random.randint(point_one, string_len)
-        new_indi = IndiString(min_v=min_value, max_v=max_value)
         new_string = parent_A[0].represent[0:point_one] + parent_B[0].represent[point_one:point_two] \
                      + parent_A[0].represent[point_two:]
-        new_indi.__setRepresent__(new_string)
-        mutate(new_indi)
+        new_string = mutate(new_string)
+        new_indi = IndiString(new_string, parent_A[0].divide_point)
         return [new_indi]
     else:
-        return (parent_A) if func(parent_A) < func(parent_B) else parent_B
+        child = parent_A if func(parent_A) < func(parent_B) else parent_B
+        return child
 
 
-def mutate(individual):
-    if random.uniform(0, 1) < 1 / 16:
-        origin_string = individual.represent
-        set_string = '0b'
-        for char in origin_string[3:-1]:
-            if random.uniform(0, 1) < 1 / 16:
-                mute = '0' if char == '1' else '1'
-                set_string = set_string + mute
-            else:
-                set_string = set_string + char
-            if len(set_string) > 16:
-                individual.__setRepresent__(set_string)
+def mutate(string):
+    origin_string = string
+    set_string = ''
+    for char in origin_string[0:]:
+        if random.uniform(0, 1) < 1 / 16:
+            mute = '0' if char == '1' else '1'
+            set_string = set_string + mute
+        else:
+            set_string = set_string + char
+    return set_string
 
 
 def ccga_model(pop_list, func, epoch):
@@ -166,12 +166,9 @@ def ga_model(pop_list, func, epoch):
     fitness_min = [np.max(eval_list)]
     for i in tqdm(range(epoch)):
         # applying scaling window
+        gen_list = [pop_list[np.nanargmin(eval_list)]]
         fmin = fitness_min[0] if i < 6 else fitness_min[i - 5]
         fitness_list = fmin - eval_list
-        # fitness_list = np.int64(fitness_list >= 0)
-        # apply elitist strategy
-        gen_list = [pop_list[np.nanargmax(fitness_list)]]
-        # scaling window
         pop_list = scaling_window(fitness_list, pop_list)
         while len(gen_list) < len(init_list):
             # two-point crossover
@@ -182,53 +179,47 @@ def ga_model(pop_list, func, epoch):
         fitness_max.append(np.min(eval_list))
         fitness_min.append(np.max(eval_list))
         pop_list = gen_list
-    return pop_list, fitness_max
+    return pop_list, fitness_max, fitness_min
 
 
 if __name__ == '__main__':
-    decimal_point = 4
     pupulation_size = 100
     num_parameter = 20
     min_value = -5.12
     max_value = 5.12
-    init_list = init_pop(population_size=100, min_v=min_value, max_v=max_value, n=num_parameter)
-    pop_list_ga, fitness_ga = ga_model(init_list, Rastrigin, 100000)
-    np.save('Rastrigin_ga.npy', np.array(fitness_ga))
-    pop_list_ccga, fitness_ccga = ccga_model(init_list, Rastrigin, 100000)
+    init_list = init_pop(population_size=100, n=num_parameter)
+    pop_list_ga, fitness_max, fitness_min = ga_model(init_list, Rastrigin, 1000)
+    np.save('Rastrigin_ga.npy', np.array(fitness_max))
+    pop_list_ccga, fitness_ccga = ccga_model(init_list, Rastrigin, 1000)
     np.save('Rastrigin_ccga.npy', np.array(fitness_ccga))
 
-
-    decimal_point = 2
     pupulation_size = 100
     num_parameter = 10
     min_value = -500
     max_value = 500
-    init_list = init_pop(population_size=100, min_v=min_value, max_v=max_value, n=num_parameter)
-    pop_list_ga, fitness_ga = ga_model(init_list, Schewefel, 100000)
-    np.save('Schewefel_ga.npy', np.array(fitness_ga))
-    pop_list_ccga, fitness_ccga = ccga_model(init_list, Schewefel, 100000)
+    init_list = init_pop(population_size=100, n=num_parameter)
+    pop_list_ga, fitness_max, fitness_min = ga_model(init_list, Schewefel, 1000)
+    np.save('Schewefel_ga.npy', np.array(fitness_max))
+    pop_list_ccga, fitness_ccga = ccga_model(init_list, Schewefel, 1000)
     np.save('Schewefel_ccga.npy', np.array(fitness_ccga))
-
-
-    decimal_point = 1
+    #
     pupulation_size = 100
     num_parameter = 10
     min_value = -512
     max_value = 512
-    init_list = init_pop(population_size=100, min_v=min_value, max_v=max_value, n=num_parameter)
-    pop_list_ga, fitness_ga = ga_model(init_list, Griewank, 100000)
-    np.save('Griewank_ga.npy', np.array(fitness_ga))
-    pop_list_ccga, fitness_ccga = ccga_model(init_list, Griewank, 100000)
+    init_list = init_pop(population_size=100, n=num_parameter)
+    pop_list_ga, fitness_max, fitness_min = ga_model(init_list, Griewank, 1000)
+    np.save('Griewank_ga.npy', np.array(fitness_max))
+    pop_list_ccga, fitness_ccga = ccga_model(init_list, Griewank, 1000)
     np.save('Griewank_ccga.npy', np.array(fitness_ccga))
-
-
-    decimal_point = 5
+    #
+    #
     pupulation_size = 100
     num_parameter = 30
     min_value = -30
     max_value = 30
-    init_list = init_pop(population_size=100, min_v=min_value, max_v=max_value, n=num_parameter)
-    pop_list_ga, fitness_ga = ga_model(init_list, Ackley, 10000)
-    np.save('Ackley_ga.npy', np.array(fitness_ga))
-    pop_list_ccga, fitness_ccga = ccga_model(init_list, Ackley, 100000)
+    init_list = init_pop(population_size=100, n=num_parameter)
+    pop_list_ga, fitness_max, fitness_min = ga_model(init_list, Ackley, 1000)
+    np.save('Ackley_ga.npy', np.array(fitness_max))
+    pop_list_ccga, fitness_ccga = ccga_model(init_list, Ackley, 1000)
     np.save('Ackley_ccga.npy', np.array(fitness_ccga))
